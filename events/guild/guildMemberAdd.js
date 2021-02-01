901ab51e502f37ae01f379c75253e2c7f76d77ab
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-02-01 13:49:11 +0100

    Part 1 of branch merging
    because pain pain pain

--- events/guild/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-const { generalChannelId } = require('../../config.json');
-const { heyEnable, usrJoin } = require('../../functions/welcomeMsg.js');
-const Discord = require('discord.js');
-
-module.exports = async (client, member) => {
-    usrJoin(client, member);
-};
--- events/guild/guildMemberRemove.js
+++
@@ -1,7 +0,0 @@
-const { generalChannelId } = require('../../config.json');
-const { heyEnable, usrLeft } = require('../../functions/welcomeMsg.js');
-const Discord = require('discord.js');
-
-module.exports = async (client, member) => {
-    usrLeft(client, member);
-};

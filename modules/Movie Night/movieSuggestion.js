9a57b9511c8efe7b821d9858d0bee0ebbf612f91
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-03-10 20:36:15 +0100

    Disabled movie suggestions T_T

--- modules/Movie Night/movieSuggestion.js
+++ modules/Movie Night/movieSuggestion.js
@@ -27,12 +27,13 @@ module.exports = {
 			//.setFooter(`Suggested by ${message.member.displayName}`);
 
 		try {	
-			client.channels.cache.get(movieNightSuggestionChannelID).send(suggestionEmbed).then(async suggestionEmbed => {
+			/*client.channels.cache.get(movieNightSuggestionChannelID).send(suggestionEmbed).then(async suggestionEmbed => {
 				suggestionEmbed.react(fingerupvote);
 				suggestionEmbed.react(fingerdownvote);
 			});
 			message.react('✅');
-			message.channel.send('Your movie suggestion was registered, thank you!');
+			message.channel.send('Your movie suggestion was registered, thank you!');*/
+			message.channel.send('We\'re really sorry, but due to Discord\'s TOS, we have to cancel Movie Nights for the forseeable future. Movie suggestions have been closed down.');
 			return;
 		} catch (e) {
 			return handleError(client, 'movieSuggestion.js', 'Error on registering a movie suggestion', e);

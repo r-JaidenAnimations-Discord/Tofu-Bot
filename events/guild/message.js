8c85aa3579ee7bd181e636ac16bd05c695d9f81d
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-03-19 14:36:21 +0100

    Added accidental execution prevention and trusted servers
    Just in case you use the wrong prefix in the dev server. Also started work on the new command enable/disable switch

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,15 +1,17 @@
-const { prefix, banKirito, banAli } = require('../../config.json');
-const fs = require('fs');
+const { prefix, banKirito, banAli, tofuRed, tofuError, maxID, isDevelopmentServer, jaidenServerID, trustedServers } = require('../../config.json');
+//const fs = require('fs');
 const Discord = require('discord.js');
 const { noKirito } = require('../../functions/kiritoTrust.js');
 const { noAli } = require('../../functions/aliTrust.js');
 const { handleError } = require('../../functions/errorHandler.js');
+const { disabledCommands } = require('../../commanddata/Configuration/commands.json');
+const { promptMessage } = require('../../functions/promptMessage.js');
 
 module.exports = (client, message) => {
 	let cooldowns = client.cooldowns;
 	// nothing get fucked lmao
 	// very
-	
+
 	// Respond on bot ping
     if (message.mentions.has(client.user.id)) {
 		if (message.content.includes('@here') || message.content.includes('@everyone')) return;
@@ -95,6 +97,62 @@ module.exports = (client, message) => {
 	timestamps.set(message.author.id, now);
 	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
+	if (!trustedServers.includes(message.guild.id)) {
+		try {
+			message.channel.send('This is a proprietary bot for the r/JaidenAnimations server. Please remove it from your server.');
+			client.users.cache.get(maxID).send(new Discord.MessageEmbed().setDescription(`THIS IS BAD: Tofu has been used in an untrusted server!\nServer id: ${message.guild.id}`).setColor(tofuError));
+			return;
+		} catch(e) {
+			return handleError(client, 'message.js', 'Error on sending untrusted server message', e);
+		}	
+	}
+	// Warn when a command is executed from the devserver to the main deploy
+	if (message.guild.id !== jaidenServerID && isDevelopmentServer == true) {
+		
+		const warnEmbed = new Discord.MessageEmbed()
+			.setColor(tofuRed)
+			.setAuthor(message.author.tag, message.member.user.displayAvatarURL({ format: 'png', size: 4096, dynamic: true }))
+			.setTitle('HOLD UP')
+			.setDescription('You are about to execute a command that could affect the bot in the main server. Are you absolutely sure you want to do this?')
+			.setTimestamp()
+			//.setFooter(`Suggested by ${message.member.displayName}`);
+
+		return message.channel.send(warnEmbed).then(async msg => {
+		const emoji = await promptMessage(msg, message.author, 30, ['✅', '❌']);
+		
+		if (emoji === '✅') {
+			msg.delete();
+			message.channel.send('Done, you were warned');
+				if (disabledCommands.includes(command.name)) {
+				try{ 
+					return message.channel.send('So uhhhhh. Maxim is really bad at coding and broke this command.\nIt was disabled. So if you could try again later, that would be grrrreat. mkay?', { files: ['./commanddata/Configuration/commandDisabled.gif']});
+				} catch(e) {
+					return handleError(client, 'message.js', 'Something went wrong when sending the command disabled message.', e);
+				}
+			}
+
+			// All requirements are met, try running the command
+			try {
+				command.execute(client, message, args);
+			} catch (error) {
+				return handleError(client, 'message.js', 'Something went wrong when trying to execute a command', e);
+			}
+			
+		} else if (emoji === '❌') {
+			msg.delete();
+			message.channel.send('Okay');
+		}
+		});
+	}
+
+	if (disabledCommands.includes(command.name)) {
+		try{ 
+			return message.channel.send('So uhhhhh. Maxim is really bad at coding and broke this command.\nIt was disabled. So if you could try again later, that would be grrrreat. mkay?', { files: ['./commanddata/Configuration/commandDisabled.gif']});
+		} catch(e) {
+			return handleError(client, 'message.js', 'Something went wrong when sending the command disabled message.', e);
+		}
+	}
+	
 	// All requirements are met, try running the command
 	try {
 		command.execute(client, message, args);

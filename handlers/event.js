bf2229c8be3c17cf6660bf977ea67b30e747fdc7
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-03-20 22:30:15 +0100

    Load as table

--- handlers/event.js
+++ handlers/event.js
@@ -2,6 +2,11 @@
 
 const fs = require('fs'); // fs is the package we need to read all files which are in folders
 
+const ascii = require('ascii-table');
+
+let table = new ascii('Commands');
+table.setHeading('Command', 'Load Status');
+
 module.exports = client => {
 	const load = dirs => {
 		const events = fs.readdirSync(`./events/${dirs}/`).filter(d => d.endsWith('.js'));
@@ -9,10 +14,12 @@ module.exports = client => {
 			const evt = require(`../events/${dirs}/${file}`);
 			let eName = file.split('.')[0];
 			client.on(eName, evt.bind(null, client));
-			console.log(eName + ' loaded');
+			//console.log(eName + ' loaded');
+			table.addRow(eName, '✔   Loaded');
 		}
 	};
 	['client', 'guild'].forEach(x=>load(x));
+	console.log(table.toString());
 };
 //wait might run be because the module export of the event has to be different too?
 // don't know how bind works with object methods x d 

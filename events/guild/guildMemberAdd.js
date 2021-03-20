8ee4735e7a9e6b25fc4c7fd01c021a78951a738e
MaxTechnics <58440941+MaxTechnics@users.noreply.github.com>
2021-03-20 19:38:59 +0100

    New welcome system

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,7 +1,28 @@
-const { usrJoin } = require('../../functions/welcomeMsg.js');
-const { jaidenServerID } = require('../../config.json');
+const fs = require('fs')
+const { jaidenServerID, generalChannelId, rulesChannelID, devMode } = require('../../config.json');
+const { handleError } = require('../../functions/errorHandler.js');
 
 module.exports = async (client, member) => {
-	if (member.guild.id != jaidenServerID) return console.log('joined but not jaidenserver');
-	usrJoin(client, member);
+	if (member.guild.id != jaidenServerID && devMode === false) return console.log('joined but not jaidenserver');
+
+	const data = await fs.readFileSync('./commanddata/Configuration/settings.json', 'utf-8');
+	var settingsFile = JSON.parse(data);
+	var welcomerState = settingsFile.welcome;
+
+	// tried it in a separate file, didn't work, this is my botch
+	const welcomes = [
+		`<@${member.id}> burst through the window for welcome!`,
+		`Hello, <@${member.id}>. Welcome to r/JaidenAnimations!`,
+		`Hello, <@${member.id}>, still waiting for plont...`,
+		`Hello <@${member.id}> grab a cup o' water and talk with us`
+	];
+
+	if (welcomerState === false) return;
+	try {
+		let randomWelc = welcomes[Math.floor(Math.random() * welcomes.length)];
+		client.channels.cache.get(generalChannelId).send(`${randomWelc}\nPlease make sure to read <#${rulesChannelID}> and the pinned comments / topics for this and other channels.\nAnd for the context, Jaiden isn't here :p`);
+		return;
+	} catch(e) {
+		return handleError(client, 'guildMemberAdd.js', 'Error on sending welcome message', e);
+	}
 };
--- events/guild/guildMemberRemove.js
+++ events/guild/guildMemberRemove.js
@@ -1,7 +1,27 @@
-const { usrLeft } = require('../../functions/welcomeMsg.js');
-const { jaidenServerID } = require('../../config.json');
+const fs = require('fs');
+const { jaidenServerID, generalChannelId, rulesChannelID, devMode } = require('../../config.json');
+const { handleError } = require('../../functions/errorHandler.js');
 
 module.exports = async (client, member) => {
-	if (member.guild.id != jaidenServerID) return console.log('joined but not jaidenserver');
-	usrLeft(client, member);
+	if (member.guild.id != jaidenServerID && devMode === false) return console.log('left but not jaidenserver');
+
+	const data = await fs.readFileSync('./commanddata/Configuration/settings.json', 'utf-8');
+	var settingsFile = JSON.parse(data);
+	var welcomerState = settingsFile.welcome;
+
+	// tried it in a separate file, didn't work, this is my botch
+	const byes = [
+		`Welps, guess like **${member.displayName}** couldn't stand to be around us, adiós.`,
+		`**${member.displayName}** is hanging up now, bye.`
+	];
+
+	if (welcomerState === false) return;
+	try {
+		let randomBye = byes[Math.floor(Math.random() * byes.length)];
+		client.channels.cache.get(generalChannelId).send(randomBye);
+		return;
+	} catch(e) {
+		return handleError(client, 'guildMemberRemove.js', 'Error on sending cya message', e);
+	}
+
 };

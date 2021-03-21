62a31af23473acac9e6b88e4bff8772bb1cd1f9b
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-03-21 10:45:18 +0100

    Implement chalk

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,14 +1,15 @@
 //const Discord = require('discord.js');
+const chalk = require('chalk');
 const { randomStatus } = require('../../functions/statusFunction.js');
-//const { staffChatId } = require('../../config.json');
+//const { staffChatID } = require('../../config.json');
 const { handleError } = require('../../functions/errorHandler.js');
 
 module.exports = client => {
 	randomStatus(client);
-	console.log(`Alive as ${client.user.tag}`);
+	console.log(chalk.green(`Alive as ${client.user.tag}`));
 
 	try {
-		//client.channels.cache.get(staffChatId).send(`I'm back b!tches!`);
+		client.channels.cache.get(staffChatID).send(`I'm back b!tches!`);
 	} catch (e) {
 		return handleError(client, 'ready.js', 'Error on sending I\'m back', e);
 	}
--- functions/errorHandler.js
+++ functions/errorHandler.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const { maxID, tofuError } = require('../config.json');
-//const chalk = require('chalk');
+const chalk = require('chalk');
 
 const handleError = (client, file, text, e) => {
 	try {
-		console.error(`${file}: ${text}: ${e}`);
+		console.log(chalk.yellow(`${file}: ${text}: ${e}`));
 		return client.users.cache.get(maxID).send(new Discord.MessageEmbed().setDescription(`BIG OOF: ${file}: ${text} \n\`\`${e}\`\``).setColor(tofuError));
 	} catch (f) {
-		console.error(`Sending error DM failed! DMError: ${f}`);
+		console.log(chalk.redBright(`Sending error DM failed! DMError: ${f}`));
 		return;
 	}
 

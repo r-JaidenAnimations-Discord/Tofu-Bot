68f038193250c8b08fbb6c68834448d689376e5c
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-03-21 18:49:54 +0100

    Fixed ping error

--- modules/Bot/ping.js
+++ modules/Bot/ping.js
@@ -7,22 +7,20 @@ module.exports = {
 	category: 'Bot',
 	usage: 'ping',
 	description: 'Ping!',
-	isEnabled: true,
-	isDMAllowed: false,
+	isDMAllowed: true,
 	isDeprecated: false,
 	aliases: ['delay', 'latency'],
 	cooldown: 5,
 	execute: async function(client, message, args) {
 		try {
 			const msg = await message.channel.send('Pinging...');
+			try {
+				msg.edit(`Pong!\nLatency: ${Math.floor(msg.createdAt - message.createdAt)}ms\nAPI Latency (Bot): ${client.ws.ping}ms`);
+			} catch (e) {
+				handleError(client, 'ping.js', 'Error on editing ping message', e);
+			}
 		} catch (e) {
 			handleError(client, 'ping.js', 'Error on sending ping message', e);
-		}
-		
-		try {
-			msg.edit(`Pong!\nLatency: ${Math.floor(msg.createdAt - message.createdAt)}ms\nAPI Latency (Bot): ${client.ws.ping}ms`);
-		} catch (e) {
-			handleError(client, 'ping.js', 'Error on editing ping message', e);
 		}
 	},
 };

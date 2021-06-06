4a129ec3cd1ba2d2c94984850b0e44800f98206f
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-06 23:35:34 +0200

    New role ID

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -4,9 +4,9 @@ const Tantrum = require('#tantrum');
 module.exports = async (client, oldMember, newMember) => {
     const { staffChatID } = client.config;
 
-    if (newMember.roles.cache.get('839177048173576272') && newMember.id === maxID) {
+    if (newMember.roles.cache.get('851198783975194674') && newMember.id === maxID) {
         try {
-            newMember.roles.remove('839177048173576272');
+            newMember.roles.remove('851198783975194674');
             client.channels.cache.get(staffChatID).send('<@640369291220353053> I SWEAR TO F*CKING GOD DO NOT EVER GIVE ME YOUR STUPID ROLE AGAIN. DON\'T EVEN TRY. ALLRIGHT.\n GOD F*CKING DAMMIT.');
         } catch (e) {
             throw new Tantrum(client, 'guildMemberUpdate.js', 'Error on removing harly role', e);

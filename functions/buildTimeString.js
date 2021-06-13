50359c2546dc2a867bd43285b395d3c76c63efba
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-13 12:56:10 +0200

    Remove abandoned timecode generation functions

--- functions/buildTimeString.js
+++ functions/buildTimeString.js
@@ -23,39 +23,6 @@ const humanReadableDuration = (msDuration) => {
 	return `${finalHourString}${finalMinuteString}${seconds}`;
 }
 
-/**
- * Builds time code
- * @param {object} data The data to build time code from
- * @returns {String}
- */
-const buildTimeCode = (data) => {
-	const items = Object.keys(data);
-	const required = ['days', 'hours', 'minutes', 'seconds'];
-	const parsed = items.filter((x) => required.includes(x)).map((m) => (data[m] > 0 ? data[m] : ''));
-	const final = parsed
-		.filter((x) => !!x)
-		.map((x) => x.toString().padStart(2, '0'))
-		.join(':');
-	return final.length <= 3 ? `0:${final.padStart(2, '0') || 0}` : final;
-}
-
-/**
- * Parses ms time
- * @param {Number} milliseconds Time to parse
- * @returns {TimeData}
- */
-const parseMS = (milliseconds) => {
-	const roundTowardsZero = milliseconds > 0 ? Math.floor : Math.ceil;
-	return {
-		days: roundTowardsZero(milliseconds / 86400000),
-		hours: roundTowardsZero(milliseconds / 3600000) % 24,
-		minutes: roundTowardsZero(milliseconds / 60000) % 60,
-		seconds: roundTowardsZero(milliseconds / 1000) % 60
-	};
-}
-
 module.exports = {
-	buildTimeCode,
-	parseMS,
 	humanReadableDuration
 }

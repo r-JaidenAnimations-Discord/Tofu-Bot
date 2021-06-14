2097c11abd68b51704368fc2a3cb4dbd8c4d7dee
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-15 00:38:52 +0200

    Added JSDoc

--- functions/createBar.js
+++ functions/createBar.js
@@ -1,22 +1,30 @@
+/**
+ * @param {Number} total Total value of the bar (maximum value)
+ * @param {Number} current Current value of the bar
+ * @param {String} size Length of the bar
+ * @param {String} line Character defenition as bar
+ * @param {String} slider Character defenition as indicator
+ * @returns {String} String built out of line characters and an indicator
+ */
 const createBar = (total, current, size = 40, line = '▬', slider = '🔵') => {
-    if (!total) throw new Error('Total value is either not provided or invalid');
-    if (!current && current !== 0) throw new Error('Current value is either not provided or invalid');
-    if (isNaN(total)) throw new Error('Total value is not an integer');
-    if (isNaN(current)) throw new Error('Current value is not an integer');
-    if (isNaN(size)) throw new Error('Size is not an integer');
-    if (current > total) {
-        const bar = line.repeat(size + 2);
-        const percentage = (current / total) * 100;
-        return [bar, percentage];
-    } else {
-        const percentage = current / total;
-        const progress = Math.round((size * percentage));
-        const emptyProgress = size - progress;
-        const progressText = line.repeat(progress).replace(/.$/, slider);
-        const emptyProgressText = line.repeat(emptyProgress);
-        const bar = progressText + emptyProgressText;
-        const calculated = percentage * 100;
-        return [bar, calculated];
-    }
+	if (!total) throw new Error('Total value is either not provided or invalid');
+	if (!current && current !== 0) throw new Error('Current value is either not provided or invalid');
+	if (isNaN(total)) throw new Error('Total value is not an integer');
+	if (isNaN(current)) throw new Error('Current value is not an integer');
+	if (isNaN(size)) throw new Error('Size is not an integer');
+	if (current > total) {
+		const bar = line.repeat(size + 2);
+		const percentage = (current / total) * 100;
+		return [bar, percentage];
+	} else {
+		const percentage = current / total;
+		const progress = Math.round((size * percentage));
+		const emptyProgress = size - progress;
+		const progressText = line.repeat(progress).replace(/.$/, slider);
+		const emptyProgressText = line.repeat(emptyProgress);
+		const bar = progressText + emptyProgressText;
+		const calculated = percentage * 100;
+		return [bar, calculated];
+	}
 };
 module.exports = createBar;
--- functions/pluralize.js
+++ functions/pluralize.js
@@ -1,3 +1,8 @@
+/**
+ * @param {String} word The word to be possibly pluralized
+ * @param {Number} times How many times the word references
+ * @returns {String} Word either with or without 's'
+ */
 const pluralizeWordOnly = (word, times) => {
     if (times === 1) {
         return word;
@@ -6,6 +11,12 @@ const pluralizeWordOnly = (word, times) => {
     }
 }
 
+/**
+ * 
+ * @param {String} word The word to be possibly pluralized
+ * @param {Number} times How many times the word references
+ * @returns {String} Number of how many times the word references and word either with or without 's'
+ */
 const pluralizeWithNumber = (word, times) => {
     if (times === 1) {
         return `${times} ${word}`

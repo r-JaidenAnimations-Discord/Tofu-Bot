2b3e39147691569fbe0860a4961bd71e3a383691
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-06-17 20:30:53 +0200

    Fix merge conflicts
    Co-Authored-By: TeraBaito <59809717+TeraBaito@users.noreply.github.com>

--- modules/Movie Night/suggestionApprove.js
+++ modules/Movie Night/suggestionApprove.js
@@ -19,9 +19,22 @@ module.exports = {
 
 		if (!checkBanStaff(client, message)) return;
 
+<<<<<<< Updated upstream
 		const id = parseInt(args[0]);
         if (!args[0] || isNaN(id)) return message.channel.send('Please specify a valid suggestion ID.');
         const reason = args.slice(1).join(' ');
+=======
+<<<<<<< Updated upstream
+		let movieID = args[0];
+		let reason = args.slice(1).join(' ');
+		if (!movieID) return message.channel.send('Invalid arguments');
+		if (!reason) reason = 'No reason specified';
+=======
+		const id = parseInt(args[0]);
+		if (!args[0] || isNaN(id)) return message.channel.send('Please specify a valid suggestion ID.');
+		const verdictReason = args.slice(1).join(' ') || 'No reason specified';
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 		const suggestion = await client.movieSuggestions.findOne({ where: { id } });
 		if (!suggestion) return message.channel.send(`Looks like an invalid ID, check your spelling.`);
@@ -30,12 +43,30 @@ module.exports = {
 
 		let newData = {
 			status: 'Approved',
+<<<<<<< Updated upstream
 			verdicter: message.author.username
 		};
 		if (reason) newData.reason = reason;
 		const affectedRows = await suggestion.update(newData);
 
 		if (affectedRows > 0) {
+=======
+			verdicter: message.author.username,
+			verdicterID: message.author.id,
+<<<<<<< Updated upstream
+			verdictReason: reason
+		}, { where: { id: movieID } });
+		if (affectedRows > 0) {
+			// I have to refetch the suggestion to get it's new info
+			let suggestion = await client.movieSuggestions.findOne({ where: { id: movieID } });
+
+=======
+			verdictReason
+		};
+		try {
+			await suggestion.update(newData);
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 			const approvalEmbed = new Discord.MessageEmbed()
 				.setColor(suggestionApproved)
 				.setTitle(`**${await suggestion.movie}**`)
@@ -54,7 +85,8 @@ module.exports = {
 			} catch (e) {
 				message.channel.send('Couldn\'t update the suggestion message, maybe it was deleted?');
 			}
-		} else {
+		} catch (e) { // If you need to log any error put (e) as param and console.error(e) before returning
+			console.error(e);
 			return message.channel.send('Something went wrong while updating the database.');
 		}
 	},

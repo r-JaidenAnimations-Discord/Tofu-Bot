e74efa2f4d9d9d01d5c817b68a423ee0d3a34ac0
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-07-03 18:39:31 +0200

    Fix staff checks

--- utils/staffChecks.js
+++ utils/staffChecks.js
@@ -8,7 +8,7 @@ const Tantrum = require('#tantrum');
  * @returns {Boolean} Returns true if message.author can ban members
  */
 const checkBanStaff = (client, message) => {
-	if (!message.member.hasPermission('BAN_MEMBERS')) {
+	if (!message.member.permissions.has('BAN_MEMBERS')) {
 		message.channel.send('You fool, need more permissions').catch(e => { throw new Tantrum(client, 'staffChecks.js', 'Error while sending ban permission message', e) });
 		return false;
 	}
@@ -22,7 +22,7 @@ const checkBanStaff = (client, message) => {
  * @returns {Boolean} Returns true if message.author can manage messages
  */
 const checkMessageStaff = (client, message) => {
-	if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+	if (!message.member.permissions.has('MANAGE_MESSAGES')) {
 		message.channel.send('You fool, need more permissions').catch(e => { throw new Tantrum(client, 'staffChecks.js', 'Error while sending message permission message', e) });
 		return false;
 	}

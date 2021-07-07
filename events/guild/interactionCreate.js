85a105981ac45a48fadee0d137056be521df9282
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-07-07 23:23:57 +0200

    Remove console log

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -2,8 +2,6 @@ module.exports = async (client, interaction) => {
     if (!interaction.member || interaction.member.partial)
         interaction.member = await interaction.guild.members.fetch(message);
 
-    console.log(interaction)
-
     if (interaction.isCommand())
         client.interactions.get(interaction.commandName).execute(client, interaction);
 };

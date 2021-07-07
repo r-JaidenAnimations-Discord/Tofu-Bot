d077c152368d9740f9b32ac74052125c2a6b64ec
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-07-08 00:29:56 +0200

    Intents

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const { Player } = require('discord-player');
 const Tantrum = require('#tantrum');
 const chalk = require('chalk');
 const client = new Discord.Client({
-	intents: [Discord.Intents.ALL] //TODO: Properly Intent
+	intents: [
+		Discord.Intents.FLAGS.DIRECT_MESSAGES,
+		Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+		Discord.Intents.FLAGS.GUILDS,
+		Discord.Intents.FLAGS.GUILD_BANS,
+		Discord.Intents.FLAGS.GUILD_EMOJIS,
+		Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+		Discord.Intents.FLAGS.GUILD_INVITES,
+		Discord.Intents.FLAGS.GUILD_MEMBERS,
+		Discord.Intents.FLAGS.GUILD_MESSAGES,
+		Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+		Discord.Intents.FLAGS.GUILD_PRESENCES,
+		Discord.Intents.FLAGS.GUILD_VOICE_STATES
+	]
 });
 const { randomStatus } = require('#utils/statusFunction.js');
 const { remindShrimp } = require('#utils/shrimpReminder.js');

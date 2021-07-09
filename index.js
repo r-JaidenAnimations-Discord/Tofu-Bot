1a1747f849ee19f06ccbe509a244ca9a0b13bf73
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-07-09 18:04:49 +0200

    Comment out possibly unneeded intents

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const client = new Discord.Client({
 		Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
 		Discord.Intents.FLAGS.GUILDS,
 		Discord.Intents.FLAGS.GUILD_BANS,
-		Discord.Intents.FLAGS.GUILD_EMOJIS,
-		Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+		// Discord.Intents.FLAGS.GUILD_EMOJIS,
+		// Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
 		Discord.Intents.FLAGS.GUILD_INVITES,
 		Discord.Intents.FLAGS.GUILD_MEMBERS,
 		Discord.Intents.FLAGS.GUILD_MESSAGES,
 		Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-		Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+		// Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
 		Discord.Intents.FLAGS.GUILD_PRESENCES,
 		Discord.Intents.FLAGS.GUILD_VOICE_STATES
 	]

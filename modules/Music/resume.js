e68c4ea48af027021bc8559ed2208898a984adae
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-07 14:07:41 +0200

    Finalize resume for discord-player v5

--- modules/Music/resume.js
+++ modules/Music/resume.js
@@ -15,7 +15,6 @@ module.exports = {
 	//aliases: [],
 	cooldown: 0,
 	execute: async function(client, message, args) {
-		return message.channel.send('Sorry, this isn\'t ready for use yet, check back in later!');
 		if (!checkMusic(client, message)) return;
 		if (!checkQueueExists(client, message)) return;
 
@@ -26,7 +25,7 @@ module.exports = {
 				.setColor(tofuOrange)
 				.setDescription('The music is already playing!');
 
-			return message.channel.send({ embeds: [alreadyPlayingEmbed] }).catch(e => { // TODO: Embedify and test
+			return message.channel.send({ embeds: [alreadyPlayingEmbed] }).catch(e => {
 				throw new Tantrum(client, 'resume.js', 'Error on sending alreadyPlayingEmbed', e)
 			});
 		}

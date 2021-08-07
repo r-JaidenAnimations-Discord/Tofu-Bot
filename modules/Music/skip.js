ff5ed4c45c98a00ad00af05dde9618ec4cd2f0ab
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-07 13:39:35 +0200

    Update skip command for discord-player v5

--- modules/Music/skip.js
+++ modules/Music/skip.js
@@ -13,13 +13,12 @@ module.exports = {
 	aliases: ['sk', 'next'],
 	cooldown: 0,
 	execute: async function(client, message, args) {
-		return message.channel.send('Sorry, this isn\'t ready for use yet, check back in later!');
 		if (!checkMusic(client, message)) return;
 		if (!checkQueueExists(client, message)) return;
 
 		const queue = client.player.getQueue(message.guild);
 
-		if (queue.skip()) { // TODO: test once v5 queue is properly working
+		if (queue.skip()) {
 			await message.react('👌').catch(e => {
 				throw new Tantrum(client, 'skip.js', 'Error on sending skip message', e);
 			});

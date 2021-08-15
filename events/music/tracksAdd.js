3752d40133e70feb593f01af8125f113497c3658
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-16 00:13:21 +0200

    Indentation fixes

--- events/music/tracksAdd.js
+++ events/music/tracksAdd.js
@@ -2,11 +2,11 @@ const { tofuGreen } = require('#colors');
 const Discord = require('discord.js');
 
 module.exports = (client, queue, tracks) => {
-    const bulkQueuedEmbed = new Discord.MessageEmbed()
-        .setColor(tofuGreen)
-        .setDescription(`Queued **${tracks.length}** tracks`);
+	const bulkQueuedEmbed = new Discord.MessageEmbed()
+		.setColor(tofuGreen)
+		.setDescription(`Queued **${tracks.length}** tracks`);
 
-    queue.metadata.channel.send({ embeds: [bulkQueuedEmbed] }).catch(e => {
-        throw new Tantrum(client, 'tracksAdd.js', 'Error sending bulkQueuedEmbed', e);
-    });
+	queue.metadata.channel.send({ embeds: [bulkQueuedEmbed] }).catch(e => {
+		throw new Tantrum(client, 'tracksAdd.js', 'Error sending bulkQueuedEmbed', e);
+	});
 }
--- modules/Music/seek.js
+++ modules/Music/seek.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: 'seek',
-    helpTitle: 'Seek',
-    category: 'Music',
-    usage: 'not yet',
-    description: 'Coming soon: seek trough the current track!',
-    isDeprecated: false,
-    isDangerous: false,
-    isHidden: false,
-    // aliases: [],
-    cooldown: 0,
-    execute: async function(client, message, args) {
-        return client.commands.get('help').execute(client, message, ['seek']);
-    },
+	name: 'seek',
+	helpTitle: 'Seek',
+	category: 'Music',
+	usage: 'not yet',
+	description: 'Coming soon: seek trough the current track!',
+	isDeprecated: false,
+	isDangerous: false,
+	isHidden: false,
+	// aliases: [],
+	cooldown: 0,
+	execute: async function(client, message, args) {
+		return client.commands.get('help').execute(client, message, ['seek']);
+	},
 };

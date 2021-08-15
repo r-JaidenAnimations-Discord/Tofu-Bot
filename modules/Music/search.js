8f5d1cf2480cb82c5b37751d1bf642d0b5ad0b10
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-16 00:11:39 +0200

    Properly handle song selection
    All bugs fixed and i did it myself!!!!

--- modules/Music/search.js
+++ modules/Music/search.js
@@ -17,7 +17,6 @@ module.exports = {
 	aliases: ['src', 'find'],
 	cooldown: 0,
 	execute: async function(client, message, args) {
-		message.channel.send('Search might or might not work propely yet');
 		if (!checkMusic(client, message)) return;
 
 		if (!args[0]) {
@@ -52,90 +51,37 @@ module.exports = {
 		message.channel.send(`\`\`\`nim\n${searchResultString}\n\`\`\``).catch(e => {
 			throw new Tantrum(client, 'searchResults.js', 'Error on sending searchResults', e);
 		}).then(async msg => {
-			const filter = async (content) => {
+			const collector = message.channel.createMessageCollector({ time: 10000 });
+
+			collector.on('collect', async ({ content }) => {
+				if (content === 'cancel') return collector.stop('cancelled');
+
 				if (!isNaN(content) && parseInt(content) >= 1 && parseInt(content) <= tracks.length) {
+					collector.stop('success');
 					const index = parseInt(content, 10);
 					const track = tracks[index - 1];
-					const queue = constructQueue(client, message);
+					const queue = await constructQueue(client, message);
 
 					queue.addTrack(track);
 					if (!queue.playing) await queue.play();
 					return track;
+
 				} else {
 					const searchInvalidResponseEmbed = new Discord.MessageEmbed()
 						.setColor(tofuOrange)
-						.setDescription(`Response has to be a valid number between **1** and **${tracks.length}**! Search cancelled.`);
+						.setDescription(`Response has to be a valid number between **1** and **${tracks.length}**!`);
 					msg.channel.send({ embeds: [searchInvalidResponseEmbed] });
 				}
-			}
-			msg.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-				.then(track => {
-					console.log('i think this is on success?');
-				})
-				.catch(collected => {
+			});
+
+			collector.on('end', (_, reason) => {
+				if (reason === 'time' || reason === 'cancelled') {
 					const embed = new Discord.MessageEmbed()
 						.setColor(tofuOrange)
-						.setDescription('Search was cancelled.');
+						.setDescription('Search cancelled.');
 					msg.channel.send({ embeds: [embed] });
-				})
+				}
+			});
 		});
-
-
-
-
-		// Reference code, djs 12
-		// const collectorString = `${message.author.id}-${message.channel.id}`;
-		// const currentCollector = this._resultsCollectors.get(collectorString);
-		// if (currentCollector) currentCollector.stop();
-
-		// const collector = message.channel.createMessageCollector((m) => m.author.id === message.author.id, {
-		// 	time: 60000
-		// });
-
-		// this._resultsCollectors.set(collectorString, collector);
-
-		// this.emit(PlayerEvents.SEARCH_RESULTS, message, query, tracks, collector);
-
-		// collector.on('collect', ({ content }) => {
-		// 	if (content === 'cancel') {
-		// 		collector.stop();
-		// 		return this.emit(PlayerEvents.SEARCH_CANCEL, message, query, tracks);
-		// 	}
-
-		// 	if (!isNaN(content) && parseInt(content) >= 1 && parseInt(content) <= tracks.length) {
-		// 		const index = parseInt(content, 10);
-		// 		const track = tracks[index - 1];
-		// 		collector.stop();
-		// 		resolve(track);
-		// 	} else {
-		// 		this.emit(PlayerEvents.SEARCH_INVALID_RESPONSE, message, query, tracks, content, collector);
-		// 	}
-		// });
-
-		// collector.on('end', (_, reason) => {
-		// 	if (reason === 'time') {
-		// 		this.emit(PlayerEvents.SEARCH_CANCEL, message, query, tracks);
-		// 	}
-		// });
-
 	},
 };
-
-
-
-// Reference code, djs 12, searchinvalidresponse event
-// let searchInvalidResponseEmbed = new Discord.MessageEmbed()
-// .setColor(tofuOrange);
-
-// if (content === 'cancel') {
-// collector.stop();
-// searchInvalidResponseEmbed.setDescription('Search was cancelled.');
-// return message.channel.send({ embeds: [searchInvalidResponseEmbed] }).catch(e => { // TODO: test
-// 	throw new Tantrum(client, 'searchInvalidResponse.js', 'Error on sending searchInvalidResponseEmbed (cancelled)', e);
-// });
-// } else {
-// searchInvalidResponseEmbed.setDescription(`Please enter a valid number between **1** and **${tracks.length}**!`);
-// message.channel.send({ embeds: [searchInvalidResponseEmbed] }).catch(e => { // TODO: test
-// 	new Tantrum(client, 'searchInvalidResponse.js', 'Error on sending searchInvalidResponseEmbed (invalid number)', e);
-// });
-// }

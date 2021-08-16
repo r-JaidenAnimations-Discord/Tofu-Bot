a0c22b4542df8d55b743ecfdf67e88cca51d59dd
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-16 20:43:19 +0200

    Add guild arg to publish slashies
    Tofu is in multiple servers
    so publishing slashies trough evil eval is now possible
    yesyes

--- utils/publishInteractions.js
+++ utils/publishInteractions.js
@@ -1,11 +1,16 @@
 const { maxID } = require('#memberIDs');
 const chalk = require('chalk');
 
-const publishInteractions = async (client) => {
+/**
+ * Clears and publishes the bot's slash commands to a given guild ID
+ * @param {Client} client Discord client object
+ * @param {String} guild Guild ID to publish slashies to
+ */
+const publishInteractions = async (client, guild) => {
 	try {
-		await client.guilds.cache.get(client.config.jaidenServerID).commands.set([]); // Reset all the command data
+		await client.guilds.cache.get(guild).commands.set([]); // Reset all the command data
 		for (let interaction of [...client.interactions.filter(({ data: { name } }) => !client.config.disabledInteractions.includes(name)).values()]) {
-			const command = await client.guilds.cache.get(client.config.jaidenServerID).commands.create(interaction.data);
+			const command = await client.guilds.cache.get(guild).commands.create(interaction.data);
 			// a thing to know: add staffOnly AND set data.defaultPermission to false
 			if (interaction.staffOnly) command.permissions.add({
 				permissions: [
@@ -18,7 +23,7 @@ const publishInteractions = async (client) => {
 			});
 			else if (interaction.permissions) command.permissions.add({ permissions: interaction.permissions });
 		}
-		console.info(chalk.green('[Info]:'), 'Slashies have been (re)published successfully!');
+		console.info(`${chalk.green('[Info]')}:`, 'Slashies have been (re)published successfully!');
 	} catch (e) {
 		console.error(e);
 	}

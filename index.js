07c00f13c6acbfe1016698ac6de026d38376ecc0
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-16 20:39:07 +0200

    Clean up error stuff
    That was a fscking mess. now it is not how sad

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const { Player } = require('discord-player');
-const Tantrum = require('#tantrum');
 const chalk = require('chalk');
 const client = new Discord.Client({
 	intents: [
@@ -58,28 +57,19 @@ switch (launchArgs[0]) {
 }
 
 // Log in
-client.login(client.config.apiKey);
-
+client.login(client.config.apiKey).catch(e => {
+	console.error(`${chalk.redBright('[Error]')}:`, e.stack);
+	process.exit(1);
+});
 
 //if sh!t goes wrong
-client.on('rateLimit', r => {
-	console.warn(`${chalk.yellow('[RATELIMIT]')}: ${r}`);
-	console.log(r);
-});
-client.on('warn', w => {
-	console.warn(`${chalk.yellow('[Warn]')}: ${w}`);
-	new Tantrum(client, 'index.js', '[WARN]: Unspecified warning', w);
-});
-client.on('error', e => {
-	console.error(`${chalk.redBright('[ERROR]')}: ${e.stack}`);
-	new Tantrum(client, 'index.js', `[ERROR]: Unspecified error: ${e.stack}`, e);
-});
-process.on('uncaughtException', e => console.error(`${chalk.redBright('[Error]')}: ${e.stack}`));
-process.on('unhandledRejection', e => {
-	console.error(`${chalk.redBright('[Error]')}: ${e.stack}`);
-	//new Tantrum(client, 'index.js', `[ERROR]: unhanled rejection: ${e}`, e.stack);
-});
-process.on('warning', e => console.warn(`${chalk.yellow('[Error]')}: ${e.stack}`));
+if (client.config.devMode) client.on('debug', d => console.log(`${chalk.cyan('[Debug]')}:`, d)); // Debug stuff, only loads when running in debug mode
+client.on('rateLimit', r => console.warn(`${chalk.yellow('[Ratelimit]')}:`, r));
+client.on('warn', w => console.warn(`${chalk.yellow('[Warn]')}:`, w));
+client.on('error', e => console.error(`${chalk.redBright('[Error]')}:`, e.stack));
+process.on('uncaughtException', e => console.error(`${chalk.redBright('[Error]')}:`, e.stack));
+process.on('unhandledRejection', e => console.error(`${chalk.redBright('[Error]')}:`, e.stack));
+process.on('warning', e => console.warn(`${chalk.yellow('[Error]')}:`, e.stack));
 
 // Handlers' modules
 ['commands', 'event', 'music', 'interaction'].forEach(handler => {

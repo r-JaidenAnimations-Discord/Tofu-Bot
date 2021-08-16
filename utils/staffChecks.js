c9069f54ac2b0899a32c96e8130cf0798a8e7c21
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-16 14:39:57 +0200

    Fixed wrong string
    I only got like 7 reports from people that it was wrong.......... oops
    Fixed the JSDoc while i was at it i guess

--- utils/staffChecks.js
+++ utils/staffChecks.js
@@ -5,7 +5,7 @@ const Tantrum = require('#tantrum');
 /**
  * Check if the message author can ban members
  * @param {Client} client Discord client
- * @param {Object} message Message object
+ * @param {Message} message Message object
  * @returns {Boolean} Returns true if message.author can ban members
  */
 const checkBanStaff = (client, message, returnMessage) => {
@@ -19,7 +19,7 @@ const checkBanStaff = (client, message, returnMessage) => {
 /**
  * Check if the message author can manage messages
  * @param {Client} client Discord client
- * @param {Object} message Message object
+ * @param {Message} message Message object
  * @returns {Boolean} Returns true if message.author can manage messages
  */
 const checkMessageStaff = (client, message, returnMessage) => {
@@ -33,12 +33,12 @@ const checkMessageStaff = (client, message, returnMessage) => {
 /**
  * Check if the message author is one of the masters
  * @param {Client} client Discord client
- * @param {*} message Message object
+ * @param {Message} message Message object
  * @returns {Boolean} Returns true if message.author is one of the masters
  */
 const masterCheck = (client, message) => {
 	if (message.author.id !== teraID && message.author.id !== maxID && message.author.id !== gradyID && message.author.id !== retainedID) {
-		message.channel.send('No dude. I don\'t want anyone but my masters mess with code in the bot...').catch(e => {
+		message.channel.send('You are not worthy enough to use this command, **perish**').catch(e => {
 			throw new Tantrum(client, 'staffChecks.js', 'Error while sending master permission message', e);
 		});
 		return false;

f840678741cd1f02e3015d3ff104143cdae1065b
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-16 20:42:33 +0200

    sweep
    throw out old sh!t

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,12 +1,9 @@
-// const Discord = require('discord.js');
-// const Tantrum = require('#tantrum');
 const chalk = require('chalk');
 const { pluralizeWithNumber } = require('#utils/pluralize.js');
 const { randomStatus } = require('#utils/statusFunction.js');
 const { publishInteractions } = require('#utils/publishInteractions.js');
 
 module.exports = async (client) => {
-
 	randomStatus(client);
 	console.log(chalk.green(`Alive as ${client.user.tag}\nOn ${pluralizeWithNumber('guild', client.guilds.cache.size)}\nAnnoying ${pluralizeWithNumber('hooman', client.users.cache.size)}`));
 
@@ -14,12 +11,5 @@ module.exports = async (client) => {
 	client.movieSuggestions.sync();
 
 	// --register slashies
-	if (process.argv.includes('--register')) await publishInteractions(client);
-
-	/*try {
-		client.channels.cache.get(staffChdatID).send(`I'm back b!tches!`);
-	} catch (e) {
-		throw new Tantrum(client, 'ready.js', 'Error on sending I\'m back', e);
-	}*/
-
+	if (process.argv.includes('--register')) await publishInteractions(client, client.config.jaidenServerID);
 }

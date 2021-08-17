9663a01a5ef2845233c8ee20b4d42d4c44c548ea
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-17 23:30:34 +0200

    Write a manifesto
    showerthoughts
    this rq

--- events/music/trackEnd.js
+++ events/music/trackEnd.js
@@ -1,3 +1,9 @@
 module.exports = (queue, track) => {
 	// TODO: Implement?
+	/**
+	 * Ok so here's an idea
+	 * when a track ends, we delete the now playing message
+	 * so how do we get started here..
+	 * I think i need to pass the message id trough something in the queue
+	 */
 }

da28369701df4efe754e359a8922af6916ebe679
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-17 23:30:10 +0200

    vibecheck slashie
    yesyes an actual slashie this time, none of that test stuff. and it does things as well even

---
+++ interactions/slVibeCheck.js
@@ -0,0 +1,27 @@
+const Tantrum = require('#tantrum');
+
+module.exports = {
+    data: {
+        name: 'vibecheck',
+        description: 'Are you vibin?',
+        options: [{
+            name: 'user',
+            description: 'Check if someone else is vibin',
+            type: 'USER',
+            required: false
+        }]
+    },
+    execute: async (client, interaction) => {
+        const user = interaction.options.get('user')?.value || interaction.member.id;
+
+        if (Math.floor(Math.random() * 10 > 3))
+            interaction.reply(`<@${user}> is vibin!`).catch(e => {
+                throw new Tantrum(client, 'slVibeCheck.js', 'Error on sending is vibin message', e);
+            });
+        else {
+            interaction.reply(`<@${user}> is not vibin!`).catch(e => {
+                throw new Tantrum(client, 'slVibeCheck.js', 'Error on sending is not vibin message', e);
+            });
+        }
+    }
+};

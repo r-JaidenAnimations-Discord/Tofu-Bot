98566cf8d407a2921d5412e3eb7cba82680a61c5
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-17 23:29:23 +0200

    Thanos snap some white lines
    poof

--- modules/Bot/changelog.js
+++ modules/Bot/changelog.js
@@ -13,7 +13,6 @@ module.exports = {
 	aliases: ['cl', 'changes'],
 	cooldown: 1,
 	execute: async function(client, message, args) {
-
 		if (args[0] === 'list') {
 			let versionList = '';
 			for (let i = 0; i < releases.length; i++) versionList += '`' + releases[i].version + '`    ';
@@ -24,7 +23,6 @@ module.exports = {
 
 		if (!release) return message.channel.send('Couldn\'t find any releases matching your query...');
 
-
 		let additionList = '';
 		let fixList = '';
 		let removalList = '';
@@ -32,7 +30,6 @@ module.exports = {
 		for (let m = 0; m < release.additions.length; m++) additionList += '+ ' + release.additions[m] + '\n';
 		for (let a = 0; a < release.fixes.length; a++) fixList += '✓ ' + release.fixes[a] + '\n';
 		for (let x = 0; x < release.removals.length; x++) removalList += '- ' + release.removals[x] + '\n';
-
 
 		const log = `**Tofu Bot ${release.version}${release === releases[releases.length - 1] ? ' (Latest)' : ''}**\nChanges:\n\`\`\`diff\n${additionList}${fixList}${removalList}\n\`\`\``
 

4d7fda05d2049379cfaab7140baba12902b5ff6d
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-08-23 03:00:47 +0200

    Add lyrics command
    Yes it's finally here now.
    Also brings back the pagination utll for like the 4th time now, but this time i actually refactored it to work properly. yay me

---
+++ modules/Music/lyrics.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const { tofuGreen, tofuError, tofuOrange } = require('#colors');
+const Genius = require('genius-lyrics');
+const { checkMusic, checkQueueExists } = require('#utils/musicChecks.js');
+const { loadingString } = require('#utils/funnyLoad.js');
+const Pagination = require('#utils/pagination.js');
+
+module.exports = {
+	name: 'lyrics',
+	helpTitle: 'Lyrics',
+	category: 'Music',
+	usage: 'lyrics',
+	description: 'Sing along',
+	isDMAllowed: false,
+	isDeprecated: false,
+	isDangerous: false,
+	mainServerOnly: false,
+	isHidden: false,
+	aliases: ['ly', 'lyr', 'lr'],
+	cooldown: 0,
+	execute: async function(client, message, args) {
+		const { lyricsToken } = client.config;
+
+		if (!checkMusic(client, message)) return;
+		if (!checkQueueExists(client, message)) return;
+		const lyricsClient = new Genius.Client(lyricsToken);
+
+		const queue = client.player.getQueue(message.guild);
+
+		const lyricsEmbed = new Discord.MessageEmbed()
+			.setTitle(`Lyrics for ${queue.current.title}`)
+			.setColor(tofuOrange)
+			.setDescription(loadingString());
+
+		const sentMessage = await message.channel.send({ embeds: [lyricsEmbed] });
+
+		let track = queue.current;
+		if (!track) return;
+		track = track.title.toLowerCase()
+			.replace(/\(lyrics|lyric|official music video|official video hd|official video|audio|official|clip officiel|clip|extended|hq\)/g, '');
+
+		const lyr = await lyricsClient.songs.search(track).then(x => x[0]);
+
+		const lyricsText = await lyr.lyrics().catch(e => {
+			lyricsEmbed.setColor(tofuError);
+			lyricsEmbed.setDescription('No lyrics were found. sad');
+			sentMessage.edit({ embeds: [lyricsEmbed] });
+			return false;
+		});
+		if (lyricsText) {
+			const lyricsChunks = Pagination.generateChunks(lyricsText, 1024);
+			lyricsEmbed.setColor(tofuGreen);
+			lyricsEmbed.setDescription(`${lyricsChunks[0]}`);
+			lyricsEmbed.setFooter(`Page 1 of ${lyricsChunks.length}.`);
+			sentMessage.edit({ embeds: [lyricsEmbed] });
+			if (lyricsChunks.length > 1) await Pagination.paginate(sentMessage, message.author, lyricsChunks);
+		}
+
+	},
+};
---
+++ utils/pagination.js
@@ -0,0 +1,66 @@
+class Pagination {
+	/**
+	 * Splits up a long string into an array of shorter strings with a given max length
+	 * @param {String} string Long string to split up into chunks
+	 * @param {Number} size Max length per chunk
+	 * @returns {Array} Array of strings with a max length of size
+	 */
+	static generateChunks(string, size) {
+		const temp = [];
+		for (let i = 0; i < string.length; i += size) temp.push(string.slice(i, i + size));
+		return temp;
+	}
+
+	static get paginationEmojis() {
+		return ['◀', '⛔', '▶'];
+	}
+
+	/**
+	 * 
+	 * @param {Message} msg Sent message object
+	 * @param {User} author Message author
+	 * @param {Array} chunks The chunks that will become different pages
+	 * @param {Boolean} toInit Whether the pagination thing needs to be initialized (add reactions)
+	 * @param {Number} currPage Which chunk is currently visible
+	 */
+	static async paginate(msg, author, chunks, toInit = true, currPage = 0) {
+		const d = async () => new Promise(r => setTimeout(r, 260));
+
+		if (toInit) for (const emoji of this.paginationEmojis) {
+			await msg.react(emoji);
+			await d();
+		}
+
+		const filter = (reaction, user) => this.paginationEmojis.includes(reaction.emoji.name) && user.id === author.id;
+
+		const collector = msg.createReactionCollector({ filter, max: 1, idle: 10000 });
+
+		collector.on('collect', async (reaction) => {
+			await reaction.users.remove(author);
+			await d(); // Wait before continuing to avoid ratelimits
+
+			const emoji = reaction.emoji.name;
+			if (emoji === this.paginationEmojis[0]) currPage--;
+			if (emoji === this.paginationEmojis[1]) {
+				await d();
+				return await msg.reactions.removeAll();
+			}
+			if (emoji === this.paginationEmojis[2]) currPage++;
+			currPage = ((currPage % chunks.length) + chunks.length) % chunks.length;
+
+			const embed = msg.embeds[0].setDescription(chunks[currPage]).setFooter(`Page ${currPage + 1} of ${chunks.length}.`);
+
+			msg.edit({ embeds: [embed] });
+
+			this.paginate(msg, author, chunks, false, currPage); // If not stopped, restarts a new collector
+		});
+		collector.on('end', async (_, reason) => {
+			if (['user', 'idle'].includes(reason)) {
+				await d();
+				await msg.reactions.removeAll();
+			}
+		});
+	}
+}
+
+module.exports = Pagination;

b92a02334e2b479c81307224f0f55bf5622ad6fc
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-09-30 20:50:19 +0200

    Linting
    My ol mac can't handle eslint while editing.... oops

--- modules/Tags/tagList.js
+++ modules/Tags/tagList.js
@@ -2,96 +2,96 @@ const { tofuBlue } = require('#colors')
 const Discord = require('discord.js')
 
 module.exports = {
-    name: 'listtags',
-    helpTitle: 'List tags',
-    category: 'Tags',
-    usage: 'listtags',
-    description: 'Get a list of all tags',
-    isDMAllowed: false,
-    isDeprecated: false,
-    isDangerous: false,
-    mainServerOnly: true,
-    isHidden: false,
-    aliases: ['tlist', 'tagslist', 'tags-list', 'list-tags', 'taglist', 'tag-list', 'list-tag', 'tags'],
-    cooldown: 0,
-    execute: async function(client, message, args) {
+	name: 'listtags',
+	helpTitle: 'List tags',
+	category: 'Tags',
+	usage: 'listtags',
+	description: 'Get a list of all tags',
+	isDMAllowed: false,
+	isDeprecated: false,
+	isDangerous: false,
+	mainServerOnly: true,
+	isHidden: false,
+	aliases: ['tlist', 'tagslist', 'tags-list', 'list-tags', 'taglist', 'tag-list', 'list-tag', 'tags'],
+	cooldown: 0,
+	execute: async function(client, message, args) {
 
-        const component = new Discord.MessageActionRow()
-            .addComponents(
-                new Discord.MessageButton()
-                    .setCustomId('prev')
-                    .setLabel('Prev')
-                    .setStyle('PRIMARY'),
-                new Discord.MessageButton()
-                    .setCustomId('next')
-                    .setLabel('Next')
-                    .setStyle('PRIMARY')
-            );
+		const component = new Discord.MessageActionRow()
+			.addComponents(
+				new Discord.MessageButton()
+					.setCustomId('prev')
+					.setLabel('Prev')
+					.setStyle('PRIMARY'),
+				new Discord.MessageButton()
+					.setCustomId('next')
+					.setLabel('Next')
+					.setStyle('PRIMARY')
+			);
 
-        // Sorted tags with a "name - by user" format
-        const tags = (await client.tags.findAll({
-            attributes: ['name', 'username']
-        }))
-            .sort((a, b) => {
-                let nameA = a.name.toUpperCase();
-                let nameB = b.name.toUpperCase();
-                if (nameA < nameB) return -1;
-                if (nameA > nameB) return 1;
-                return 0;
-            })
-            .map(t => `**${t.name}**` + ' - by ' + t.username);
+		// Sorted tags with a "name - by user" format
+		const tags = (await client.tags.findAll({
+			attributes: ['name', 'username']
+		}))
+			.sort((a, b) => {
+				let nameA = a.name.toUpperCase();
+				let nameB = b.name.toUpperCase();
+				if (nameA < nameB) return -1;
+				if (nameA > nameB) return 1;
+				return 0;
+			})
+			.map(t => `**${t.name}**` + ' - by ' + t.username);
 
-        /**
-         * Splits an array every x elements
-         * from stackoverflow lolllllll
-         * @param {Array} arr 
-         * @param {Number} size Every how much it'll split
-         * @returns {Array}
-         */
-        function paginate(arr, size) {
-            return arr.reduce((acc, val, i) => {
-                let idx = Math.floor(i / size);
-                let page = acc[idx] || (acc[idx] = []);
-                page.push(val);
+		/**
+		 * Splits an array every x elements
+		 * from stackoverflow lolllllll
+		 * @param {Array} arr 
+		 * @param {Number} size Every how much it'll split
+		 * @returns {Array}
+		 */
+		function paginate(arr, size) {
+			return arr.reduce((acc, val, i) => {
+				let idx = Math.floor(i / size);
+				let page = acc[idx] || (acc[idx] = []);
+				page.push(val);
 
-                return acc;
-            }, []);
-        }
+				return acc;
+			}, []);
+		}
 
-        // Current page (args[0] or 0), if it's less it returns
-        let curPage = parseInt(args[0]) - 1 || 0;
-        if (curPage < 0) return message.channel.send('Please enter a valid page number!');
-        let pages = paginate(tags, 10);
-        const embed = new Discord.MessageEmbed()
-            .setTitle('All Tags')
-            .setDescription(pages[curPage].join('\n'))
-            .setFooter(`Page ${curPage + 1} of ${pages.length}`)
-            .setColor(tofuBlue);
+		// Current page (args[0] or 0), if it's less it returns
+		let curPage = parseInt(args[0]) - 1 || 0;
+		if (curPage < 0) return message.channel.send('Please enter a valid page number!');
+		let pages = paginate(tags, 10);
+		const embed = new Discord.MessageEmbed()
+			.setTitle('All Tags')
+			.setDescription(pages[curPage].join('\n'))
+			.setFooter(`Page ${curPage + 1} of ${pages.length}`)
+			.setColor(tofuBlue);
 
-        const msg = await message.channel.send({ embeds: [embed], components: [component] });
-        const collector = msg.createMessageComponentCollector({
-            filter: i => i.user.id == message.author.id,
-            idle: 30000
-        });
-        collector.on('collect', button => {
-            switch (button.customId) {
-                case 'prev': {
-                    curPage - 1 < 0 ? curPage = pages.length - 1 : curPage--;
-                    break;
-                }
-                case 'next': {
-                    curPage + 1 >= pages.length ? curPage = 0 : curPage++;
-                    break;
-                }
-            }
-            embed.setDescription(pages[curPage].join('\n'));
-            embed.setFooter(`Page ${curPage + 1} of ${pages.length}`);
-            msg.edit({ embeds: [embed] });
-            button.reply('this horribleness you have to do').then(() => button.deleteReply());
-        });
-        collector.on('end', () => {
-            msg.edit({ components: [] })
-        })
+		const msg = await message.channel.send({ embeds: [embed], components: [component] });
+		const collector = msg.createMessageComponentCollector({
+			filter: i => i.user.id == message.author.id,
+			idle: 30000
+		});
+		collector.on('collect', button => {
+			switch (button.customId) {
+				case 'prev': {
+					curPage - 1 < 0 ? curPage = pages.length - 1 : curPage--;
+					break;
+				}
+				case 'next': {
+					curPage + 1 >= pages.length ? curPage = 0 : curPage++;
+					break;
+				}
+			}
+			embed.setDescription(pages[curPage].join('\n'));
+			embed.setFooter(`Page ${curPage + 1} of ${pages.length}`);
+			msg.edit({ embeds: [embed] });
+			button.reply('this horribleness you have to do').then(() => button.deleteReply());
+		});
+		collector.on('end', () => {
+			msg.edit({ components: [] })
+		})
 
-    }
+	}
 };

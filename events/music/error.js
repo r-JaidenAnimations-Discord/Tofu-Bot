9d69daea5d596dae7ffbb9dc1685791c3760c558
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-10-05 22:18:18 +0200

    Make it not Tantrum when 403 or 404 status
    I desperately should learn how to use lavalink

--- events/music/error.js
+++ events/music/error.js
@@ -3,13 +3,23 @@ const Discord = require('discord.js');
 const Tantrum = require('#tantrum');
 
 module.exports = (client, queue, error, ...args) => {
-	const errorEmbed = new Discord.MessageEmbed()
-		.setColor(tofuError)
-		.setDescription(`Tofu choked :headstone:\n\`${error}\``)
-		.setFooter('This error has been automatically reported to the devs')
-		.setTimestamp();
-	new Tantrum(client, 'error.js', 'Tofu Choked', `${error}`);
-	queue.metadata.channel.send({ embeds: [errorEmbed] }).catch(e => {
-		new Tantrum(client, 'error.js', 'Error on sending errorEmbed', e);
-	});
+	switch (error) {
+		case 'Status code: 403':
+			queue.metadata.channel.send('Status 403, not allowed to get that resource (this is a known issue and will be fixed soon, hopefully');
+			break;
+		case 'Status code: 404':
+			queue.metadata.channel.send('Status 404, resource not found');
+			break;
+		default:
+			const errorEmbed = new Discord.MessageEmbed()
+				.setColor(tofuError)
+				.setDescription(`Tofu choked :headstone:\n\`${error}\``)
+				.setFooter('This error has been automatically reported to the devs')
+				.setTimestamp();
+			new Tantrum(client, 'error.js', 'Tofu Choked', `${error}`);
+			queue.metadata.channel.send({ embeds: [errorEmbed] }).catch(e => {
+				new Tantrum(client, 'error.js', 'Error on sending errorEmbed', e);
+			});
+			break;
+	}
 };

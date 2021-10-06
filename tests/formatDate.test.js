65ef66f33a5cf066d57ee7f4798ac581c18c3f36
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-10-06 18:46:45 +0200

    Change date construction
    Hopefully this generalizes the test timezone

--- tests/formatDate.test.js
+++ tests/formatDate.test.js
@@ -1,5 +1,5 @@
 const { formatDate } = require('../utils/formatDate.js');
 
 test('Formatting date', () => {
-	expect(formatDate(new Date('Tue Dec 31 2002 19:27:00 GMT+0100 (Central European Standard Time)'))).toBe('Tuesday, December 31, 2002, 7:27:00 PM');
+	expect(formatDate(new Date('Tue Dec 31 2002 19:27:00 GMT+0100'))).toBe('Tuesday, December 31, 2002, 7:27:00 PM');
 });

65bd431eb5bba47e8db86fa8a1e41e03f987b1a2
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-10-20 22:36:15 +0200

    Remove some junk

--- modules/Lavalink/lavaNightcore.js
+++ modules/Lavalink/lavaNightcore.js
@@ -16,10 +16,6 @@ module.exports = {
 		if (!LavaManager.vcChecks(client, message)) return;
 		if (!LavaManager.nodeChecks(client, message)) return;
 		if (!(await LavaManager.musicChecks(client, message))) return;
-		// client.music.filters.timescale = (client.music.nightcore = !client.music.nightcore)
-		// await createQueue().filters.timescale // = (client.music.nightcore = !client.music.nightcore)
-		// 	? { speed: 1.125, pitch: 1.125, rate: 1 }
-		// 	: undefined;
 
 		const player = await LavaManager.getPlayer(client, message);
 		player.filters.timescale = (player.nightcore = !player.nightcore)

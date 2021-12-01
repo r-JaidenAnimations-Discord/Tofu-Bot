f7a01502bb683582c74682cfa7825cb355dc9fb8
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-12-01 21:19:56 +0100

    Implement player events

---
+++ events/lava/queueFinish.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const LavaManager = require('#handlers/lavaManager.js');
+const { musicStrings } = require('#assets/global/strings.json');
+const { tofuOrange } = require('#colors');
+
+module.exports = (client, queue) => {
+	LavaManager.lavaLog('Queue finished, setting inactivity timer');
+	if (queue.player.timeout) {
+		clearTimeout(queue.player.timeout);
+	}
+	queue.player.timeout = setTimeout(p => {
+		LavaManager.lavaLog('Inactive for too long, destroying player');
+		const channel = p.queue.channel;
+		p.disconnect();
+		client.music.destroyPlayer(p.guildId);
+		const embed = new Discord.MessageEmbed()
+			.setColor(tofuOrange)
+			.setDescription(musicStrings.inactiveTimeout);
+
+		channel.send({ embeds: [embed] });
+	}, 1800, queue.player);
+};
---
+++ events/lava/trackStart.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const LavaManager = require('#handlers/lavaManager.js');
+const { tofuGreen } = require('#colors');
+
+module.exports = (client, queue, song) => {
+	LavaManager.lavaLog('Starting track');
+	queue.player.pause(false);
+	if (queue.player.timeout) {
+		LavaManager.lavaLog('Player not destroyed because inactivity. Destroying inactivity timer');
+		clearTimeout(queue.player.timeout);
+		delete queue.player.timeout;
+	}
+
+	const embed = new Discord.MessageEmbed()
+		.setColor(tofuGreen)
+		.setTitle('Now playing')
+		.setDescription(`[${song.title}](${song.uri}) [<@${song.requester}>]`);
+
+	queue.channel.send({ embeds: [embed] });
+};

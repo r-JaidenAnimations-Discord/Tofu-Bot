3fd2dd1e666b86df449743ab10a609003fd84f36
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-12-18 16:50:18 +0100

    Fix joke command
    Use local copy of the repo

--- modules/Fun/joke.js
+++ modules/Fun/joke.js
@@ -1,8 +1,6 @@
-const { tofuGreen, tofuError } = require('#colors');
+const { tofuGreen } = require('#colors');
 const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch');
-const Tantrum = require('#tantrum');
-const { loadingString } = require('#utils/funnyLoad.js');
+const jokes = require('#assets/commandJoke/jokes.json');
 
 module.exports = {
 	name: 'joke',
@@ -17,28 +15,13 @@ module.exports = {
 	// aliases: [],
 	cooldown: 5,
 	execute: async function(client, message, args) {
+		const joke = jokes.randomElement()
 		const jokeEmbed = new MessageEmbed()
 			.setColor(tofuGreen)
-			.setTitle(loadingString())
-			.setFooter('The API does not have good jokes, don\'t get your hopes up');
+			.setTitle(joke.setup)
+			.setDescription(`||${joke.punchline}||`)
+			.setFooter('These are not good jokes, don\'t get your hopes up');
 
-		const msg = await message.channel.send({ embeds: [jokeEmbed] });
-
-		// API endpoint
-		const url = 'https://official-joke-api.appspot.com/random_joke';
-
-		const APIresponse = await fetch(url).then(r => r.json()).catch(e => {
-			new Tantrum(client, e);
-		});
-
-		if (APIresponse?.setup && APIresponse?.punchline) {
-			jokeEmbed.setTitle(APIresponse.setup);
-			jokeEmbed.setDescription(`||${APIresponse.punchline}||`);
-			return msg.edit({ embeds: [jokeEmbed] });
-		}
-
-		if (msg.deletable) msg.delete();
-
-		return message.channel.send({ embeds: [new MessageEmbed().setDescription('So uh the API doesn\'t wanna talk rn').setColor(tofuError)] });
+		message.channel.send({ embeds: [jokeEmbed] });
 	},
 };
